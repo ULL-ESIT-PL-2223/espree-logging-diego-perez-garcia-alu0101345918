c6fc3974f9c90b4e4d17369cdd0071d411e9e73c
Diego Perez Garcia <72458271+DiegoPerezGarcia@users.noreply.github.com>
2023-03-02 03:01:39 +0000

    Change option to a better name

--- bin/log.js
+++ bin/log.js
@@ -11,7 +11,7 @@ program
   .version(version)
   .argument("<filename>", 'file with the original code')
   .option("-o, --output <filename>", "file in which to write the output")
-  .option("-e --exec", "execute the output program")
+  .option("-s --show", "show result in console")
   .action((filename, options) => {
     transpile(filename, options.output, options.exec);
   });
